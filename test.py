6198e23ef40123624f4825bc740d80c65db2c338
v-furukawa <56102175+v-furukawa@users.noreply.github.com>
2022-09-05 12:48:18 +0900

    バージョン情報ファイル追加

--- test.py
+++ test.py
@@ -4,13 +4,12 @@ from unittest.mock import patch
 
 path = './hello.txt'
 path_w = './hello2.txt'
+path_3 = './version.txt'
 
-with open(path) as f:
+with open(path_3) as f:
   s = f.read()
-  print(type(s))
-  print(s)
 
-string = 'New File'
+s += 1
 
-with open(path_w, mode='w') as f2:
-  f2.write(string)
+with open(path_3, mode='w') as f2:
+  f2.write(s)
